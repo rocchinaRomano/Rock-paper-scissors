61ed5dd47d15c24ea6b16f5e03b04144bf252c4a
rocchinaRomano <rocchina.romano@yahoo.it>
2023-01-09 11:38:47 +0100

    Upd test

--- test_game.py
+++ test_game.py
@@ -12,10 +12,7 @@ class TestGame(unittest.TestCase):
 		choise_player1 = 'R'
 		choise_player2 = 'R'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: DRAW!\n"
+		result = "DRAW"
 
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 		
@@ -27,28 +24,22 @@ class TestGame(unittest.TestCase):
 		choise_player1 = 'P'
 		choise_player2 = 'P'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: DRAW!\n"
+		result = "DRAW"
 
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
-	
-		self.assertEqual(result, result_server)		
+		
+		self.assertEqual(result, result_server)
 	
 	def test_get_winner_draw3(self):
 		
 		choise_player1 = 'S'
 		choise_player2 = 'S'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: DRAW!\n"
+		result = "DRAW"
 
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
-	
-		self.assertEqual(result, result_server)		
+		
+		self.assertEqual(result, result_server)
 
 	
 	def test_get_winner_rock1(self):
@@ -56,85 +47,65 @@ class TestGame(unittest.TestCase):
 		choise_player1 = 'R'
 		choise_player2 = 'S'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player1 + " won "+ self.name_player2 + "!\n"
-
+		result = self.name_player1
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
 		self.assertEqual(result, result_server)		
+
 	
 	def test_get_winner_rock2(self):
 		# [R]ock breaks [S]cissors ->  [R]ock wins
 		choise_player1 = 'S'
 		choise_player2 = 'R'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player2 + " won "+ self.name_player1 + "!\n"
-
+		result = self.name_player2
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
 		self.assertEqual(result, result_server)		
+
+	
 	
 	def test_get_winner_scissors1(self):
 		# [S]cissors cut [P]aper -> [S]cissors win	
 		choise_player1 = 'S'
 		choise_player2 = 'P'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player1 + " won "+ self.name_player2 + "!\n"
-
+		result = self.name_player1
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
 		self.assertEqual(result, result_server)		
 
+	
 	def test_get_winner_scissors2(self):
 		# [S]cissors cut [P]aper -> [S]cissors win	
 		choise_player1 = 'P'
 		choise_player2 = 'S'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player2 + " won "+ self.name_player1 + "!\n"
-
+		result = self.name_player2
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
-		self.assertEqual(result, result_server)
+		self.assertEqual(result, result_server)		
+
 
 	def test_get_winner_paper1(self):
 		#[P]aper wraps [R]ock -> [P]aper wins
 		choise_player1 = 'P'
 		choise_player2 = 'R'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player1 + " won "+ self.name_player2 + "!\n"
-
+		result = self.name_player1
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
-		self.assertEqual(result, result_server)
+		self.assertEqual(result, result_server)		
 	
 	def test_get_winner_paper2(self):
 		#[P]aper wraps [R]ock -> [P]aper wins
 		choise_player1 = 'R'
 		choise_player2 = 'P'
 
-		result = "\n***FINAL RESULTS***\n\n"
-		result = result + "[Player 1: " + self.name_player1 + " - Choise: " + choise_player1 + "]\n" 
-		result = result + "[Player 2: " + self.name_player2 + " - Choise: " + choise_player2 + "]\n"
-		result = result + "\nRESULT: " + self.name_player2 + " won "+ self.name_player1 + "!\n"
-
+		result = self.name_player2
 		result_server = get_winner(choise_player1, choise_player2, self.name_player1, self.name_player2)
 
-		self.assertEqual(result, result_server)
-		
-
+		self.assertEqual(result, result_server)		
+	
 if __name__ == "__main__":
 	unittest.main()
